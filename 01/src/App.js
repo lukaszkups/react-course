26841137c7f6b4f90abedf2910bd0fcb68c4f949
mrmnmly <mrmnmly@gmail.com>
2020-01-22 15:34:44 +0100

    Created reusable component with slot/props.children functionality.

--- 01/src/App.js
+++ 01/src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <h1 className="App-title">I am a react app</h1>
         <p>It's working!</p>
-        <Person />
+        <Person name="Max" age="30" />
+        <Person name="Cara" age="32">My hobbies: Racing</Person>
+        <Person name="Oke" age="22" />
       </div>
     );
     // the same
--- 01/src/Person/Person.js
+++ 01/src/Person/Person.js
@@ -1,8 +1,13 @@
 import React from 'react' // this needs to be imported to parse jsx!
 
 // my first functional component (no need to import {Component} from react library)
-const person = () => {
-  return <h1>I'm a person!</h1>
+const person = (props) => {
+  return (
+    <div>
+      <p>I'm a {props.name} and I'm {props.age} years old!</p>
+      <p>{props.children}</p>
+    </div>
+  );
 }
 
 export default person;
