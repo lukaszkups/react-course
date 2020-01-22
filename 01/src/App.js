64646bdcc7f684d005be5aa223190485ab86b463
mrmnmly <mrmnmly@gmail.com>
2020-01-22 15:29:03 +0100

    Creating functional components.

--- 01/src/App.js
+++ 01/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1 className="App-title">Welcomeee</h1>
-    //   </div>
-    // );
+    return (
+      <div className="App">
+        <h1 className="App-title">I am a react app</h1>
+        <p>It's working!</p>
+        <Person />
+      </div>
+    );
     // the same
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', {className: 'App-title'}, 'Welcomeee'));
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', {className: 'App-title'}, 'Welcomeee'));
   }
 }
 
---
+++ 01/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react' // this needs to be imported to parse jsx!
+
+// my first functional component (no need to import {Component} from react library)
+const person = () => {
+  return <h1>I'm a person!</h1>
+}
+
+export default person;
